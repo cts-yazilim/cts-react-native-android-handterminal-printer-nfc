27e0e1a8e262c885d932e75cb604570e4457c4a6
ctsyazilim <48091710+ctsyazilim@users.noreply.github.com>
2019-05-03 11:36:47 +0300

    Update PrinterData.java

--- android/src/main/java/com/handterminalprinternfc/PrinterData.java
+++ android/src/main/java/com/handterminalprinternfc/PrinterData.java
@@ -5,10 +5,15 @@ import java.util.List;
 
 public class PrinterData {
 
+	Boolean GunSonuMu = new Boolean(false);
 	String FisBaslik = new String();
 	String EksperAdi= new String();
 	String AlimYeriAdi = new String();
 	String FabrikaAdi = new String();
+	Double ToplamAlimKg = new Double(0);
+	Double ToplamKesintiKg = new Double(0);
+	Double ToplamNetKg= new Double(0);
+
 	List<Alim> Alimlar ;
 }
 
